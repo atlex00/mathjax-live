2e6d27f5625c3ede94399322e7df05b9e27bcc6c
atlex00 <38640461+atlex00@users.noreply.github.com>
2022-02-20 00:33:01 +0100

    Test

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ input.addEventListener("input", updateValue);
 function updateValue(e) {
   console.log(e.target.value)
   rendered.textContent = e.target.value;
-  MathJax.Hub.Queue(["Typeset", MathJax.Hub, "render"]);
+  MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
 }
